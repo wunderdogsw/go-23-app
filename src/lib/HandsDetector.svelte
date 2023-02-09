ff63008edbda648b1ffa21200b2a7be90cc8b30c
Ido Schacham <ido.schacham@wunderdog.fi>
2023-02-09 14:23:44 +0100

    fix: use lite tfjs with max hands to improve performance

--- src/lib/HandsDetector.svelte
+++ src/lib/HandsDetector.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
   import { onDestroy, onMount, setContext, getContext } from 'svelte'
 
-  import * as handPoseDetection from '@tensorflow-models/hand-pose-detection'
   import '@tensorflow/tfjs-core'
   import '@tensorflow/tfjs-backend-webgl'
-  import '@mediapipe/hands'
+  import * as handPoseDetection from '@tensorflow-models/hand-pose-detection'
 
   import { key } from './hands-detector'
   import { key as videoKey } from './camera-video'
@@ -16,8 +15,9 @@
     const model = handPoseDetection.SupportedModels.MediaPipeHands
     const detectorConfig = {
       // mediapipe runtime doesn't work when building :(
+      maxHands: 2,
       runtime: 'tfjs',
-      modelType: 'full',
+      modelType: 'lite',
     }
     return await handPoseDetection.createDetector(model, detectorConfig)
   }
