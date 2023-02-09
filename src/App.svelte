801dc488f78dace36c87c2f5a22112b12fa5efc3
Ido Schacham <ido.schacham@wunderdog.fi>
2023-02-09 11:12:11 +0100

    refactor: components in app

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,15 @@
 <script lang="ts">
   import CameraVideo from './lib/CameraVideo.svelte'
-  // import FaceTracker from './lib/ClmTracker.svelte'
-  import TensorFlow from './lib/CircleHands.svelte'
+  import CircleHands from './lib/CircleHands.svelte'
   import Canvas from './lib/Canvas.svelte'
   import HandsDetector from './lib/HandsDetector.svelte'
 </script>
 
 <main>
   <CameraVideo>
-    <!--    <FaceTracker />-->
     <Canvas>
       <HandsDetector>
-        <TensorFlow />
+        <CircleHands />
       </HandsDetector>
     </Canvas>
   </CameraVideo>
