5db9c989e79c42b8076c609dc3d1f2a6ec6ced1d
Ido Schacham <ido.schacham@wunderdog.fi>
2023-02-09 12:06:15 +0100

    fix: building for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  // remove absolute path so that the build can also work on GitHub pages
+  base: '',
 })
