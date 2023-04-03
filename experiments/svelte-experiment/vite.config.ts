f3e8b7649a0597f39b8986337523d42dc2f976bb
Hannu Hagelberg <hannu.hagelberg@wunderdog.fi>
2023-04-03 14:11:45 +0200

    Moving bubbleone content to the root & deleting experiments & deleting shapes.html & shapes.js

--- experiments/svelte-experiment/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [svelte()],
-  // remove absolute path so that the build can also work on GitHub pages
-  base: '',
-})
