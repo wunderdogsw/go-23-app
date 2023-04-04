f58946d1edc32f80d16ad190ae09a323cf597572
Ido Schacham <ido.schacham@wunderdog.fi>
2023-04-04 11:00:41 +0200

    refactor: controls (#86)

---
+++ js/controls.js
@@ -0,0 +1,67 @@
+import { getSelectedVideoInputDeviceId, getVideoInputDevices } from './media.js';
+import { getParameters, setDefaultParameters, setParameters } from './parameters.js';
+import { convertFormToJson, createSelectOption, getQueryStringValue, setInputValueByName } from './utils.js';
+
+export async function initControls({ onSubmit }) {
+  initInputValues();
+  await initVideoInput();
+  initEventHandlers(onSubmit);
+  initToggle();
+}
+
+function initInputValues() {
+  const parameters = getParameters();
+  Object.entries(parameters).forEach(([name, value]) => setInputValueByName(name, value));
+}
+
+async function initVideoInput() {
+  const [videoInputControl] = document.getElementsByName('videoDeviceId');
+  if (!videoInputControl) {
+    console.warn(`Haven't found video control input name videoDeviceId`);
+    return;
+  }
+
+  const videoInputDevices = await getVideoInputDevices();
+  const selectedVideoDeviceId = await getSelectedVideoInputDeviceId();
+
+  videoInputDevices.forEach(({ deviceId, label }) => {
+    const option = createSelectOption(label, deviceId, selectedVideoDeviceId);
+    videoInputControl.appendChild(option);
+  });
+}
+
+function initEventHandlers(onSubmit) {
+  const controls = document.getElementById('controls');
+  controls.onsubmit = (event) => submitControlsForm(event, onSubmit);
+
+  const resetButton = document.getElementById('reset');
+  resetButton.onclick = () => resetInputValues(onSubmit);
+}
+
+function submitControlsForm(event, onSubmit) {
+  event.preventDefault();
+
+  const parameters = convertFormToJson(event.target);
+  setParameters(parameters);
+  onSubmit();
+}
+
+function resetInputValues(onSubmit) {
+  setDefaultParameters();
+  initInputValues();
+  onSubmit();
+}
+
+function initToggle() {
+  const controlsQueryString = getQueryStringValue('controls');
+  if (!!controlsQueryString) {
+    toggleControls();
+  }
+
+  hotkeys('ctrl+k, command+k', () => toggleControls());
+}
+
+function toggleControls() {
+  const controls = document.getElementById('controls');
+  controls.classList.toggle('hidden');
+}
--- js/utils.js
+++ js/utils.js
@@ -131,10 +131,26 @@ export function disposeGroup(group) {
   });
 }
 
-// reference: https://stackoverflow.com/questions/175739/how-can-i-check-if-a-string-is-a-valid-number
-function isNumeric(str) {
-  if (typeof str != 'string') return false;
-  return !isNaN(str) && !isNaN(parseFloat(str));
+export function createSelectOption(label, value, selectedValue) {
+  const option = document.createElement('option');
+  option.value = value;
+  option.textContent = label;
+
+  if (value === selectedValue) {
+    option.selected = true;
+  }
+
+  return option;
+}
+
+export function setInputValueByName(name, value) {
+  const [element] = document.getElementsByName(name);
+  if (!element) {
+    console.warn(`Didn't find input name ${name}`);
+    return;
+  }
+
+  element.value = value;
 }
 
 export function convertFormToJson(form) {
@@ -148,4 +164,10 @@ export function convertFormToJson(form) {
   }
 
   return json;
+}
+
+// reference: https://stackoverflow.com/questions/175739/how-can-i-check-if-a-string-is-a-valid-number
+function isNumeric(str) {
+  if (typeof str != 'string') return false;
+  return !isNaN(str) && !isNaN(parseFloat(str));
 }
