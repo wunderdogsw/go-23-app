6e7f83280f37381c014fa5419fc2a2e3b028d056
Ido Schacham <ido.schacham@wunderdog.fi>
2023-04-11 11:42:06 +0200

    docs: removed jsdocs

--- js/textures/index.js
+++ js/textures/index.js
@@ -4,9 +4,6 @@ import { getRandomItem } from '../utils/maths.js';
 
 export const COLOR_TEXTURES = [...COLOR_VIDEO_TEXTURES, ...COLOR_STATIC_TEXTURES];
 
-/**
- * @returns THREE.VideoTexture|THREE.Texture
- */
 export function getRandomColorTexture() {
   return getRandomItem(COLOR_TEXTURES);
 }
